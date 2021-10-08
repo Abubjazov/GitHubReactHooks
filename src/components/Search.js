9b18e8ce5ff787ed3bf506721670c5008fa59664
Radik Abubjazov <abubjazov@mail.ru>
2021-10-08 23:46:08 +0300

    Search: + получение данных с github.com

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,19 @@
 import React, { useContext, useState } from 'react'
 import { AlertContext } from '../context/alert/alertContext'
+import { GithubContext } from '../context/github/githubContext'
 
 export const Search = () => {
     const [value, setValue] = useState('')
-
     const {show} = useContext(AlertContext)
+    const github = useContext(GithubContext)
+
     const onSubmit = e => {
         if (e.key !== 'Enter') {
             return
         }
 
         if (value.trim()) {
-            console.log('make request...', value)
+            github.search(value.trim())
         } else {
             show('Enter user details!')
         }
